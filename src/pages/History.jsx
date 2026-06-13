a9379fdca16ac3b3cabdc8f9de9eb8467e09424a
marcoscarneiro202412-wq <marcoscarneiro202412@gmail.com>
2026-06-13 14:13:13 -0300

    Protecting

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -5,44 +5,52 @@ import NavBar from "../components/NavBar";
 import { deleteHistory } from "../features/auth/authSlice";
 
 function History() {
-  const {history, id} = useSelector((s) => s.auth.user);
+  const { history, id } = useSelector((s) => s.auth.user);
   const dispatch = useDispatch();
-  if (!history) return <Navigate to={"/"} />;
+  try {
+    if (!history) return <Navigate to={"/"} />;
 
-  return (
-    <div className={styles.history}>
-      <NavBar />
-      <div className={styles.items}>
-        {history.length ? history.map((h) => (
-          <div className={styles.item} key={h.id}>
-            <div>
-              <h3>{h.id}</h3>
-              <p>Rounds: {h.rounds}</p>
-              <p>Winner: {h.winner}</p>
-              <p>Data: {h.data}</p>
-            </div>
-            <button
-              style={{
-                background: "red",
-                border: "1px solid #000",
-                color: "#fff",
-                width: "25px",
-                height: "25px",
-                borderRadius: "50%",
-                aspectRatio: "1",
-                cursor: "pointer"
-              }}
-              onClick={() => {
-                dispatch(deleteHistory({historyId: h.id, playerId: id}));
-              }}
-            >
-              &times;
-            </button>
-          </div>
-        )):<p style={{color: "#fff"}}>Você Não jogou nenhuma partida</p>}
+    return (
+      <div className={styles.history}>
+        <NavBar />
+        <div className={styles.items}>
+          {history.length ? (
+            history.map((h) => (
+              <div className={styles.item} key={h.id}>
+                <div>
+                  <h3>{h.id}</h3>
+                  <p>Rounds: {h.rounds}</p>
+                  <p>Winner: {h.winner}</p>
+                  <p>Data: {h.data}</p>
+                </div>
+                <button
+                  style={{
+                    background: "red",
+                    border: "1px solid #000",
+                    color: "#fff",
+                    width: "25px",
+                    height: "25px",
+                    borderRadius: "50%",
+                    aspectRatio: "1",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => {
+                    dispatch(deleteHistory({ historyId: h.id, playerId: id }));
+                  }}
+                >
+                  &times;
+                </button>
+              </div>
+            ))
+          ) : (
+            <p style={{ color: "#fff" }}>Você Não jogou nenhuma partida</p>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch {
+    return <Navigate to={"/"} />;
+  }
 }
 
 export default History;
